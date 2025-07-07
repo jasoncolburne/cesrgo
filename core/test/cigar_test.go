5ac33d772ad829802d095bc7f4857be0e88fe3c3
Jason Colburne <j.colburne@gmail.com>
2025-07-07 18:09:02 -0300

    cigar round trip

--- core/test/cigar_test.go
+++ core/test/cigar_test.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"bytes"
+	"crypto/rand"
 	"fmt"
 	"testing"
 
@@ -46,5 +48,53 @@ func TestCigarCodesAndSizes(t *testing.T) {
 				t.Fatalf("cigar code mismatch: %s != %s", cigar.GetCode(), testVector.CigarCode)
 			}
 		})
+	}
+}
+
+func TestCigarRoundTrip(t *testing.T) {
+	raw := [64]byte{}
+	_, err := rand.Read(raw[:])
+	if err != nil {
+		t.Fatalf("failed to read random bytes: %v", err)
+	}
+
+	cigar, err := cesr.NewCigar(nil, options.WithCode(codex.Ed25519_Sig), options.WithRaw(raw[:]))
+	if err != nil {
+		t.Fatalf("failed to create cigar: %v", err)
+	}
+
+	qb2, err := cigar.Qb2()
+	if err != nil {
+		t.Fatalf("failed to get qb2: %v", err)
+	}
+
+	qb2Cigar, err := cesr.NewCigar(nil, options.WithQb2(qb2))
+	if err != nil {
+		t.Fatalf("failed to create cigar from qb2: %v", err)
+	}
+
+	qb64, err := qb2Cigar.Qb64()
+	if err != nil {
+		t.Fatalf("failed to get qb64: %v", err)
+	}
+
+	qb64Cigar, err := cesr.NewCigar(nil, options.WithQb64(qb64))
+	if err != nil {
+		t.Fatalf("failed to create cigar from qb64: %v", err)
+	}
+
+	qb64b, err := qb64Cigar.Qb64b()
+	if err != nil {
+		t.Fatalf("failed to get qb64b: %v", err)
+	}
+
+	qb64bCigar, err := cesr.NewCigar(nil, options.WithQb64b(qb64b))
+	if err != nil {
+		t.Fatalf("failed to create cigar from qb64b: %v", err)
+	}
+
+	qb64bRaw := qb64bCigar.GetRaw()
+	if !bytes.Equal(qb64bRaw, raw[:]) {
+		t.Fatalf("qb64b raw mismatch: %x != %x", qb64bRaw, raw[:])
 	}
 }
